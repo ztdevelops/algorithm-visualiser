de41db867762fdebe3c907b2407413e00f9f7511
Yue Zheng Ting <yuezhengting@gmail.com>
2023-01-04 21:30:02 +0800

    Add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import SortingVisualiser from './sort/visualise'
+import NavbarComponent from './components/navbar';
 
 function App() {
   return (
     <div className="App">
-      <SortingVisualiser></SortingVisualiser>
+      <NavbarComponent/>
+      <SortingVisualiser/>
     </div>
   );
 }
---
+++ src/components/navbar.jsx
@@ -0,0 +1,30 @@
+import 'react-bootstrap/dist/react-bootstrap';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function NavbarComponent() {
+    return (
+        <Navbar bg="light" expand="lg">
+            <Container>
+                <Navbar.Brand href="#home">Algorithm</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <NavDropdown title="Categories" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Sorting</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.2">
+                                Pathfinding
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavbarComponent;
