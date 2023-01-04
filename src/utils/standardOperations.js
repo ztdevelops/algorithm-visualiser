6ec57c1aa692eed583e60094a9f5a9aaf0d3e91e
Yue Zheng Ting <yuezhengting@gmail.com>
2023-01-05 01:49:55 +0800

    Fix compilation warnings

--- src/utils/standardOperations.js
+++ src/utils/standardOperations.js
@@ -3,9 +3,9 @@ import { temporaryButtonDisable } from "./temporaryButtonDisable";
 
 function disableSortButtonsAndUpdateInformationDOM(runtime, sortType, i) {
     // Update runtime
-    document.getElementById('runtime').innerHTML = '<div> Runtime: ' + '<b>' + `${runtime.toFixed(3)}` + '</b> ms.' + '<div>';
+    document.getElementById('runtime').innerHTML = `<div>Runtime: <b>${runtime.toFixed(3)}</b> ms.`
     // Update sortType
-    document.getElementById('sortType').innerHTML = '<div>' + TEMPLATE_SORT_MESSAGE + '<b>' + sortType + '</b>' + '<div>';
+    document.getElementById('sortType').innerHTML = `<div>${TEMPLATE_SORT_MESSAGE}<b>${sortType}</b></div>`;
     // Temporarily disable buttons
     temporaryButtonDisable(i, 'sort', TIMEOUT_DURATION);
 }
